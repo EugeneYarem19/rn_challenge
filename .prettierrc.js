5d5f10b05dc491fe15c39f1ee694e865156b56f2
Євгеній Яремченко <eugeneYarem@gmail.com>
2020-09-01 16:12:20 +0300

    TypeScript, ESLint and Prettier were configured

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,6 +1,12 @@
 module.exports = {
-  bracketSpacing: false,
-  jsxBracketSameLine: true,
-  singleQuote: true,
-  trailingComma: 'all',
+  bracketSpacing: true,
+  // jsxBracketSameLine: true,
+  useTabs: false, // использовать табы? нет (я за пробелы)
+  printWidth: 165, // длина строки - 80
+  tabWidth: 2, // длина "таба" - 2 пробела
+  singleQuote: false, // использовать одинарные кавычки - да!
+  trailingComma: "es5", // запятая в последней строке - да
+  jsxBracketSameLine: false, // закрывающийся jsx в этой же строке
+  parser: "flow", // парсер - flow (пока не важно)
+  semi: true, // точка с запятой - да
 };
