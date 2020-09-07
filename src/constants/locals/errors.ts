9e1e1e5cab9b4b93a2b1934e8cb37d23baf68736
Євгеній Яремченко <eugeneYarem@gmail.com>
2020-09-07 15:11:15 +0300

    Error messages was placed in one place

---
+++ src/constants/locals/errors.ts
@@ -0,0 +1,9 @@
+export const errors = {
+  emptyTitle: "Please, provide movie title to search",
+  infoDescription: "We cannot fetch detailed information about this movie. Error: \n",
+  infoTitle: "Something has gone wrong!",
+
+  clientError: "Bad request",
+  networkError: "Please check internet connection",
+  serverError: "Server error",
+};
