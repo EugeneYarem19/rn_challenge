46d181a907a580fe2df7121f6c0e0053528945f1
Євгеній Яремченко <eugeneYarem@gmail.com>
2020-09-08 20:04:04 +0300

    UI update

--- src/components/Poster/styles.ts
+++ src/components/Poster/styles.ts
@@ -3,16 +3,22 @@ import { Dimensions, StyleSheet, } from "react-native";
 import { palette, } from "@theme";
 
 const imageWidth = Dimensions.get("window").width - 16;
-const image = { backgroundColor: palette.canvasColor, borderRadius: 8, };
+const image = { backgroundColor: palette.canvasColor, };
 
 export const styles = StyleSheet.create({
-  poster: { ...image, },
+  poster: { ...image, overflow: "hidden", },
   placeholder: {
     ...image,
     alignItems: "center",
     flexDirection: "column",
     justifyContent: "center",
   },
-  small: { height: 180, width: 122, },
-  large: { maxHeight: 443, maxWidth: 300, height: imageWidth * 1.4, width: imageWidth, },
+  small: { borderRadius: 8, height: 180, width: 122, },
+  large: {
+    borderRadius: 20,
+    maxHeight: 443,
+    maxWidth: 300,
+    height: imageWidth * 1.4,
+    width: imageWidth,
+  },
 });
